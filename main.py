4ffd5b3bfdaf9d81530b8dbe3755aaab1d05ee21
AndreiT80 <and8016t@gmail.com>
2024-12-22 10:55:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Домашнее задание по теме "Модели SQLALchemy. Отношения между таблицами." Modul 17-2
+
+from fastapi import FastAPI
+from app.routers import task
+from app.routers import user
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message" : "Welcome to TaskManager"}
+
+app.include_router(task.router)
+app.include_router(user.router)
+
+
+# загрузка    uvicorn main:app
